99e7b2e3131c6b5634f9a91aaf9a802b067500ec
David Pires <developer.david@outlook.com>
2022-04-24 23:44:09 -0300

    fix: tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 
+
 export default function Home() {
   return (
-    <div className={`
-                    flex h-screen 
-                    justify-center items-center
-                    `}>
-      <span className="text-4xl">TEXTO</span>
+    <div className="flex h-screen justify-center items-center 
+                    bg-gradient-to-r from-purple-500 via-yellow-50  
+                    to-blue-600"
+    >
+      <span className="">Texto</span>
     </div>
   )
 }
