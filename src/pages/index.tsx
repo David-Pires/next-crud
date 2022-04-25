954299cc6a8ba91a03036c4b9cbe936355ededf8
David Pires <developer.david@outlook.com>
2022-04-25 00:18:07 -0300

    feat: new page teste1.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,11 @@
 
 export default function Home() {
   return (
-    <div className="flex h-screen justify-center items-center 
+    <div className={`flex h-screen justify-center items-center 
                     bg-gradient-to-r from-purple-500 via-yellow-50  
-                    to-blue-600"
+                    to-blue-600`}
     >
-      <span className="">Texto</span>
+      <span className="text-4xl">Texto</span>
     </div>
   )
 }
---
+++ src/pages/teste1.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Teste1() {
+  return (
+    <div>
+      <span className="text-5xl my-5">teste 1</span>
+    </div>
+  )
+}
