30435aaaa7f6a2aca521bb8bca00e7bcb0e7f97e
David Pires <developer.david@outlook.com>
2022-04-24 22:05:59 -0300

    config

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+  return (
+    <div className={`
+                    flex h-screen 
+                    justify-center items-center
+                    `}>
+      <span className="text-4xl">TEXTO</span>
+    </div>
+  )
+}
