ab0ee8dfce5ea93a996c0d3f264a963b42790a00
David Pires <developer.david@outlook.com>
2022-04-27 00:36:53 -0300

    fix: fixes new arquives

--- src/hook/useTabelaOuForm.ts
+++ src/hook/useTabelaOuForm.ts
@@ -1,5 +1,15 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function useTabelaOuForm() {
-  const [visivel, setVisivel] = useState('tabela')
+    const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
+
+    const exibirTabela = () => setVisivel('tabela')
+    const exibirFormulario = () => setVisivel('form')
+
+    return {
+        formularioVisivel: visivel === 'form',
+        tabelaVisivel: visivel === 'tabela',
+        exibirTabela,
+        exibirFormulario,
+    }
 }
