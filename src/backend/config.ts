b24707077e230b1373f22da4144017857619b570
David Pires <developer.david@outlook.com>
2022-04-26 23:53:05 -0300

    feat: Hooks, useClientes.ts useTabelaOuForm.ts

--- src/backend/config.ts
+++ src/backend/config.ts
@@ -1,8 +1,9 @@
+
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 
-if(!firebase.apps.length) {
-  firebase.initializeApp({
+if(firebase.apps.length) {
+  const = firebase.initializeApp({
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain:process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId:process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
